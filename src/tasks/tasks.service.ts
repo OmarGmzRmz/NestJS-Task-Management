6e164187f5fc7115b50577716cdfcfdc9dbbe1cc
OmarGmzRmz <gomar8138@gmail.com>
2021-09-06 15:04:31 -0500

    Added tasks repository

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { Task, TaskStatus } from './task.model';
+import { Task, TaskStatus } from './task.entity';
 import { v4 as uuid } from 'uuid';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
